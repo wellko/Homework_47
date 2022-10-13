1eb830e75cc5981a7f722272713d8ecf61bae43f
Kwon Valerii <wellko24@gmail.com>
2022-10-13 12:13:25 +0600

    task-1

--- task1.sh
+++ task1.sh
@@ -2,3 +2,5 @@
 sudo mkdir -pv projects/my_projects/directory1/subdirectory1
 sudo mkdir -pv projects/my_projects/directory3/subdirectory3
 sudo mkdir -pv projects/my_projects/directory2
+
+echo "Hello from JS" | sudo tee projects/my_projects/directory2/hello.txt

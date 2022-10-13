4d9e4558caadaea9d099160e2e997f2a9b3220ed
Kwon Valerii <wellko24@gmail.com>
2022-10-13 11:53:26 +0600

    created directory

---
+++ task1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo mkdir -pv projects/my_projects/directory1/subdirectory1
+sudo mkdir -pv projects/my_projects/directory3/subdirectory3
+sudo mkdir -pv projects/my_projects/directory2
